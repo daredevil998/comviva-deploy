42175ad63cf8f84ed0873d1fdd973b15720a253c
unknown <Administrator@MCGD-TRN029.COMVIVA.COM>
2020-02-17 14:51:05 +0530

    first

---
+++ src/main/java/com/viva/fatd/FATDApplication.java
@@ -0,0 +1,13 @@
+package com.viva.fatd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FATDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FATDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viva/fatd/FATDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viva.fatd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FATDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
